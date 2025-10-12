01424031b3567fbaeebdf477c8ffb8e35a837531
norwaa <shiuryan282@gmail.com>
2025-10-13 00:01:38 +0800

    Added browsing mode w/ load records integrated 🐈

--- goober/main.py
+++ goober/main.py
@@ -20,55 +20,10 @@ if __name__ == "__main__":
     userinput = ""
     budget_planner = []
 
-    ## LOADS CSV FILE DATA ##
-    def loadBudgetData():
-        while True:
-            userinput = input("Input CSV Filename. [FORMAT ######]   [.letmeout] - Quit\n\n") + ".csv"
-            magicword = userinput.replace(".", "").lower()
-
-            if "letmeout" in magicword and userinput[0] == ".":         ## LOOKS FOR ".lmetout"
-                return "Quit"
-
-            if os.path.exists(userinput):       ## CHECK WHETHER FILE EXISTS
-                print("# Loading CSV Data #".center(66, "_"))
-                print("")
-                break
-            print("")
-            print("# File Not Found #".center(66, "_"))
-
-        filename = str(userinput)
-        data = []
-        values = []
-        with open(filename, "r") as file:
-            obtainedheader = False
-            reader = csv.reader(file)
-            list_of_rows = list(reader)
-
-            for i in range(len(list_of_rows)):
-                if obtainedheader != True:
-                    headers = list_of_rows[i]
-                    obtainedheader = True
-                else:
-                    data.append(list_of_rows[i])
-                    if list_of_rows[i][0] == "Income":
-                        values.append(int(list_of_rows[i][2]))
-                    else:
-                        values.append(-int(list_of_rows[i][2]))
-
-        ## PRINTS TABLE ##
-        print(tabulate(data, headers=headers, tablefmt="pipe"))
-        while True:
-            userinput = input(f"\n\033[4m Monthly Spent. { sum(values) }\033[24m _________________ [R] Return [Q] Quit  ")          ## F STRINGS, ANSI ESCAPE CODES
-            if userinput.lower() == "q":
-                return "Quit"
-            elif userinput.lower() == "r":
-                print("_".center(66, "_"))
-                break
-
 
 
     ## DISPLAY ADDED RECORDS ##
-    def previewRecord(budget_planner):
+    def previewEntry(budget_planner):
         headers = ["Type","Description","Account","Date","Category"]
         data = []
         values = []
@@ -79,8 +34,7 @@ if __name__ == "__main__":
             else:
                 values.append(-int(budget_planner[i][2]))
         
-        print("")
-        print("# Record Preview #".center(66, "_")) 
+        print("# Database Preview #".center(66, "_")) 
         print("")
         print(tabulate(data, headers=headers, tablefmt="pipe"))         ## PRINTS TABLE
         while True:
@@ -88,7 +42,7 @@ if __name__ == "__main__":
             return
     
     ## ADD NEW RECORDS TO NEW CSV FILE ##
-    def addRecords():
+    def addEntry():
         
         ## LOOKS FOR THE KEYWORD "".LETMEOUT" ON EVERY INPUT ##
         def letmeout(userinput):
@@ -129,7 +83,7 @@ if __name__ == "__main__":
             new_table = []
             ## ENTER TYPE ##
             while True:
-                userinput = input("Enter Data Type. [I] Income [E] Expenses [.letmeout] Quit\n\n")
+                userinput = input("Enter Entry Type. [I] Income [E] Expenses [.letmeout] Quit\n\n")
                 if letmeout(userinput) == True:
                     return("Quit")
                 if assigndata(userinput, False) != "Invalid":
@@ -179,7 +133,9 @@ if __name__ == "__main__":
                     return("Quit")
                 if assigndata(userinput, True) != "Invalid":
                     new_table.append(assigndata(userinput,True))
-                    print("# Record Preview #".center(66, "_"))
+                    print("_".center(66, "_"))
+                    print("")
+                    print("# Entries Preview #".center(66, "="))
                     break
                 print("\nInvalid Input.")
                 print("_".center(66, "_"))
@@ -188,18 +144,19 @@ if __name__ == "__main__":
             print("")
             print(tabulate([new_table], ["Type","Description","Account","Date","Category"], tablefmt="pipe"))
             while True:
-                userinput = input("\nConfirm Values? [Y] Yes [N] No   ")
+                userinput = input("\nConfirm Entries? [Y] Yes [N] No   ")
                 if userinput.lower() == "y":
                     budget_planner.append(new_table)
                     while True:
-                        userinput = input("Add Another Record? [Y] Yes [N] No   ")
+                        userinput = input("Add Another Entry? [Y] Yes [N] No   ")
                         while True:
                             if userinput.lower() == "y":
-                                print("_".center(66, "_"))
+                                print("_".center(66, "="))
                                 return
                             elif userinput.lower() == "n":
+                                print("")
+                                print("# Database Created #".center(66, "="))
                                 return "Quit"
-                                print("_".center(66, "_"))
                             break 
                         print("Invalid Input.")
                         print("_".center(66, "_"))
@@ -216,7 +173,7 @@ if __name__ == "__main__":
     def exportcsv(budget_planner):
         while True:
             
-            userinput = input("Input CSV Filename. \n[.letmeout] - Quit\n\n") + ".csv"
+            userinput = input("Input Filename. \n[.letmeout] Quit\n\n") + ".csv"
             magicword = userinput.replace(".", "").lower()
             if "letmeout" in magicword and userinput[0] == ".":     ## Triggers when csvname contains .letmeout
                 return
@@ -245,42 +202,153 @@ if __name__ == "__main__":
                     print("_".center(66, "_"))
                     break
 
+    
+    
+    ## BROWSING DIRECTORY CSVS ##
+    def browseDatabase():
+        csvfiles = []
+        
+         ## LOADS CSV FILE DATA ##
+        def loadBudgetData():
+            print("=".center(66, "="))
+            while True:
+                print("")
+                print(tabulate(csvfiles, headers=["# Databases Found Within Directory #".center(60, " ")], tablefmt="github"))
+                print("\nCurrently - Loading Mode")
+                userinput = input("Input Filename. [FORMAT ######]   [.letmeout] Quit\n\n") + ".csv"
+                magicword = userinput.replace(".", "").lower()
+                if "letmeout" in magicword and userinput[0] == ".":         ## LOOKS FOR ".lmetout"
+                    return "Quit"
+                
+                if os.path.exists(userinput):       ## CHECK WHETHER FILE EXISTS
+                    print("# Loading CSV Data #".center(66, "_"))
+                    print("")
+                    break
+                print("")
+                print("# File Not Found #".center(66, "="))
 
+            filename = str(userinput)
+            data = []
+            values = []
+            with open(filename, "r") as file:
+                obtainedheader = False
+                reader = csv.reader(file)
+                list_of_rows = list(reader)
 
+                for i in range(len(list_of_rows)):
+                    if obtainedheader != True:
+                        headers = list_of_rows[i]
+                        obtainedheader = True
+                    else:
+                        data.append(list_of_rows[i])
+                        if list_of_rows[i][0] == "Income":
+                            values.append(int(list_of_rows[i][2]))
+                        else:
+                            values.append(-int(list_of_rows[i][2]))
+
+            ## PRINTS TABLE ##
+            print(tabulate(data, headers=headers, tablefmt="pipe"))
+            while True:
+                userinput = input(f"\n\033[4m Monthly Spent. { sum(values) }\033[24m _________________ [R] Return [Q] Quit  ")          ## F STRINGS, ANSI ESCAPE CODES
+                if userinput.lower() == "q":
+                    return "Quit"
+                elif userinput.lower() == "r":
+                    print("")
+                    break
+        
+        
+        ## DELETE FILE FROM DIRECTORY ##
+        def deleteData():
+            print("=".center(66, "="))
+            while True:
+                print("")
+                print(tabulate(csvfiles, headers=["# Databases Found Within Directory #".center(60, " ")], tablefmt="github"))
+                print("\nCurrently - Deletion Mode.")
+                userinput = input("Input Filename. [.letmeout] Quit\n\n") + ".csv"
+                magicword = userinput.replace(".", "").lower()
+                if "letmeout" in magicword and userinput[0] == ".":     ## Triggers when csvname contains .letmeout
+                    return
+                print("")
+                
+                while True:
+                    changestate = input("Confirm Filename? [DELETION CANNOT BE REVERTED.]  [Y] Yes [N] No  ")
+                    if changestate == "Y" or changestate == "y":
+                        if os.path.exists(userinput):       ## CHECK WHETHER FILE EXISTS
+                            print("")
+                            for i in range(0, len(csvfiles)):
+                                if userinput in csvfiles[i]:
+                                    csvfiles.pop(i)
+                            os.remove(userinput)
+                            print("# Database Removed #".center(66, "="))
+                        else:
+                            print("")
+                            print("# File Does Not Exist #".center(66, "="))
+                            break
+                    if changestate == "N" or changestate == "n":
+                        print("_".center(66, "_"))
+                    break
+            return    
+        
+        ## MAIN BROWSE MODE UI ##
+        counter = 0        
+        for files in os.listdir(os.path.dirname(__file__)):         ## LIST ALL CSV EXTENSION FILES
+            if files.endswith(".csv"):
+                counter += 1
+                csvfiles.append([files])
+        if counter == 0:
+            print("No Databases Are Found.")
+            return 
+        
+        while True:
+            print("")
+            print(tabulate(csvfiles, headers=["# Databases Found Within Directory #".center(60, " ")], tablefmt="github"))
+            print("\nCurrently - Overview Mode.")
+            userinput = input("[L] Load Database [D] Delete Database [.letmeout] Quit   ")
+            magicword = userinput.replace(".", "").lower()
+            if userinput.lower() == "l":
+                while True:
+                    if loadBudgetData() == "Quit":
+                        break
+                print("")
+                print("=".center(66, "="))
+            if userinput.lower() == "d":
+                deleteData()
+                print("=".center(66, "="))
+            if "letmeout" in magicword and userinput[0] == ".":     ## Triggers when csvname contains .letmeout
+                return
+        return
+
+                
     ## MAIN UI ##
     while True:
         print("")
         print("[ Kikyou TUI Budget Planner ]".center(66, "="))
         print("_".center(66, "_"))
         print("")
-        print("[1] Load CSV Database")
-        print("[2] Preview Recent Record")
-        print("[3] Add New Records")
-        print("[4] Export Record")
+        print("[1] Browse Database Library")
+        print("[2] Preview Recent Database")
+        print("[3] Create Database")
+        print("[4] Export Database As CSV")
         print("[Q] Quit")
         print("_".center(66, "_"))
         userinput = input("Waiting for input..  ")
+        print("")
 
         if userinput == "1":
-            print("=".center(66, "=")) 
-            print("")
-            while True:
-                if loadBudgetData() == "Quit":
-                    break
-            print("")
+            print("=".center(66, "="))
+            browseDatabase()
         elif userinput == "2":
             print("=".center(66, "="))    
             print("")
-            previewRecord(budget_planner)
+            previewEntry(budget_planner)
             print("")
             pass
         elif userinput == "3":
             print("=".center(66, "="))
             print("") 
             while True:
-                if addRecords() == "Quit":
+                if addEntry() == "Quit":
                     break
-            print("")
             pass
         elif userinput == "4":
             print("=".center(66, "="))    
